f61c16f399276616d96d2574263beb5be6af79be
enricomarino <enrico.marino@email.com>
2020-04-29 10:39:22 +0200

    updated container

--- src/container.js
+++ src/container.js
@@ -1,11 +1,34 @@
 'use strict'
 
-import { Component } from './component.js'
+import Component from './component.js'
 
-export class Container extends Component {
+export default class Container extends Component {
 
-  constructor () {
-    super()
+  get container () {
+    return this
+  }
+
+  attach (page) {
+    if (this.page === page) {
+      return
+    }
+    this.container.appendChild(page)
+    this.page = page
+
+    requestAnimationFrame(this.scroll)
+  }
+
+  clear () {
+    let container = this.container
+    let child
+    while (child = container.firstChild) {
+      child.remove()
+    }
+  }
+
+  scroll () {
+    document.body.scrollTo(0, 0)
+    window.scrollTo(0, 0)
   }
 
 }
