11236461217e721fa637e2c31c643d5e0bc803ae
enricomarino <enrico.marino@email.com>
2020-04-29 10:40:27 +0200

    updated index

--- src/index.js
+++ src/index.js
@@ -1,40 +1,15 @@
 'use strict'
 
-import { Director } from './director.js'
-import { DirectiveAttribute } from './directives/attribute.js'
-import { DirectiveClassName } from './directives/classname.js'
-import { DirectiveData } from './directives/data.js'
-import { DirectiveEvent } from './directives/event.js'
-import { DirectiveProperty } from './directives/property.js'
-import { DirectiveRepeat } from './directives/repeat.js'
-import { DirectiveStyle } from './directives/style.js'
-import { DirectiveToggle } from './directives/toggle.js'
-import { DirectiveText } from './directives/text.js'
-import { DirectiveHtml } from './directives/html.js'
+import Director from './director.js'
+import Component from './component.js'
+import Container from './container.js'
+import Controller from './controller.js'
+import Router from './router.js'
+import Emitter from './emitter.js'
+import Register from './register.js'
+import App from './app.js'
 
-import { Component } from './component.js'
-import { Container } from './container.js'
-import { Controller } from './controller.js'
-import { Router } from './router.js'
-import { Emitter } from './emitter.js'
-import { Register } from './register.js'
-
-import { App } from './app.js'
-
-Director.directives = [
-  DirectiveAttribute,
-  DirectiveData,
-  DirectiveClassName,
-  DirectiveEvent,
-  DirectiveProperty,
-  DirectiveRepeat,
-  DirectiveToggle,
-  DirectiveStyle,
-  DirectiveText,
-  DirectiveHtml
-]
-
-export {
+export default {
   Director,
   Component,
   Container,
