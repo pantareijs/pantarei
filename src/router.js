71197c9acc5b6c5d6b35eb145415a51a3b86e753
enricomarino <enrico.marino@email.com>
2020-04-29 10:39:43 +0200

    updated router

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
 'use strict'
 
-import { Route } from './route.js'
-import { Emitter } from './emitter.js'
+import Route from './route.js'
+import Emitter from './emitter.js'
 
-export class Router {
+export default class Router {
 
   static get default_routes () {
     return [
@@ -50,28 +50,11 @@ export class Router {
 
   constructor () {
     this.events = new Emitter()
-    this._on_hashchange = this._on_hashchange.bind(this)
   }
 
-  async start (config) {
-    if (this._started) {
-      return this
-    }
-
+  async start () {
     let routes = await this.constructor._routes
-    this._init_routes(routes)
 
-    location.hash = location.hash || '#/'
-    let url = location.pathname + location.hash
-
-    this._on_hashchange({ newURL: url })
-    window.addEventListener('hashchange', this._on_hashchange)
-
-    this._started = true
-    return this
-  }
-
-  _init_routes (routes) {
     let root = routes.find(route => route.home)
     if (!root) {
       root = routes.find(route => route.name === 'home')
@@ -85,21 +68,14 @@ export class Router {
     this.root = new Route(root)
   }
 
-  stop () {
-    window.removeEventListener('hashchange', this._on_hashchange)
-    this._started = false
-    return this
-  }
-
-  _on_hashchange (event) {
-    let url = event.newURL
-    let path = url.substr(url.indexOf('#') + 1)
-
+  navigate (path) {
     let matching = this.root.match(path)
 
-    if (matching.route) {
-      this.events.emit('change_route', matching)
+    if (!matching.route) {
+      return
     }
+
+    this.events.emit('change', matching)
   }
 
 }
