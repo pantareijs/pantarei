29c10a8e46e62ee195ad0b25b70434409e65845c
enricomarino <enrico.marino@email.com>
2020-04-29 10:39:09 +0200

    updated controller

--- src/controller.js
+++ src/controller.js
@@ -1,32 +1,11 @@
 'use strict'
 
-import { Emitter } from './emitter.js'
+import Emitter from './emitter.js'
 
-export class Controller {
+export default class Controller {
 
   constructor () {
     this.events = new Emitter()
-    this.data = {}
-  }
-
-  dispatch (props) {
-    this.events.emit('dispatch', props)
-    for (let prop_name in props) {
-      let prop = props[prop_name]
-      this.events.emit(`dispatch ${prop_name}`, prop)
-    }
-  }
-
-  update (data) {
-    Object.assign(this.data, data)
-
-    if (this.container && this.container.data) {
-      Object.assign(this.container.data, this.data)
-    }
-
-    if (this.page && this.page.data) {
-      Object.assign(this.page.data, this.data)
-    }
   }
 
 }
