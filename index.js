032d75fde18022a51be5adf13f6c8522b5d31db6
enricomarino <enrico.marino@email.com>
2020-05-26 12:47:45 +0200

    modularize

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 'use strict'
 
-import Pantarei from './src/index.js'
+import Director from './modules/director/index.js'
+import Component from './modules/component/index.js'
+import Container from './modules/container/index.js'
+import Controller from './modules/controller/index.js'
+import Router from './modules/router/index.js'
+import Navigator from './modules/navigator/index.js'
+import Emitter from './modules/emitter/index.js'
+import Register from './modules/register/index.js'
+import App from './modules/app/index.js'
+
+let Pantarei = {
+  Director,
+  Component,
+  Container,
+  Controller,
+  Router,
+  Navigator,
+  Emitter,
+  Register,
+  App
+}
 
 window['Pantarei'] = Pantarei
 
