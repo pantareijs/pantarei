7ea40c39cd9e572eae6aa2fe433ac524a94e58b7
enricomarino <enrico.marino@email.com>
2020-06-10 09:32:44 +0200

    add find_elements

--- modules/component-parts/index.js
+++ modules/component-parts/index.js
@@ -5,18 +5,20 @@ export default superclass => class extends superclass {
   async init () {
     super.init()
     await this.locks.unlocked('content')
-    this.parts = this.find_parts(this.shadowRoot)
+    this.parts = this.find_elements(this.shadowRoot, 'part')
+    this.refs = this.find_elements(this.shadowRoot, 'id')
     this.locks.unlock('parts')
   }
 
-  find_parts (node) {
-    let nodes = node.querySelectorAll('[part]').values()
+  find_elements (node, attribute_name) {
+    let selector = `[${attribute_name}]`
+    let nodes = node.querySelectorAll(selector).values()
 
-    let parts = new Map()
+    let parts = {}
 
     for (let node of nodes) {
-      let part_name = node.getAttribute('part')
-      parts.set(part_name, node)
+      let part_name = node.getAttribute(attribute_name)
+      parts[part_name] = node
     }
 
     return parts
