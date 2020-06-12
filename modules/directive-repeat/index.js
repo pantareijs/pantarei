b98499e6c66e69c9b4ec07be9cbc02c140961861
enricomarino <enrico.marino@email.com>
2020-06-12 20:52:45 +0200

    update constructor

--- modules/directive-repeat/index.js
+++ modules/directive-repeat/index.js
@@ -7,7 +7,7 @@ export default class DirectiveRepeat extends Directive {
 
   static prefix = 'repeat'
 
-  static default = {
+  static params = {
     items_path: 'data.items',
     item_name: 'item',
     index_name: 'index',
@@ -15,36 +15,18 @@ export default class DirectiveRepeat extends Directive {
     value_name: 'value'
   }
 
-  static parse (node, attribute) {
-    if (!this.match(node, attribute)) {
-      return
-    }
-
-    let key = this.parse_key(attribute)
-    let value_path = this.parse_value_path(attribute)
-    let value_expression = new Expression(value_path)
-
-    let items_path = node.getAttribute('repeat') || this.default.items_path
-    let item_name = node.getAttribute('item') || this.default.item_name
-    let index_name = node.getAttribute('index') || this.default.index_name
-    let key_name = node.getAttribute('key') || this.default.key_name
-    let value_name = node.getAttribute('value') || this.default.value_name
-
-    let directive = new this({ node, key, value_path, value_expression,
-      items_path, item_name, index_name, key_name, value_name })
-
-    return directive
-  }
-
   constructor (options) {
     super(options)
 
-    this.node = options.node
-    this.items_path = options.items_path
-    this.item_name = options.item_name
-    this.index_name = options.index_name
-    this.key_name = options.key_name
-    this.value_name = options.value_name
+    let node = this.node
+
+    let default_params = this.constructor.params
+    let params = this.params = {}
+    params.items_path = node.getAttribute('repeat') || default_params.items_path
+    params.item_name = node.getAttribute('item') || default_params.item_name
+    params.index_name = node.getAttribute('index') || default_params.index_name
+    params.key_name = node.getAttribute('key') || default_params.key_name
+    params.value_name = node.getAttribute('value') || default_params.value_name
 
     let node_name = this.node.nodeName.toLowerCase()
     if (node_name === 'template') {
@@ -119,10 +101,13 @@ export default class DirectiveRepeat extends Directive {
       return
     }
 
+    let item_name = this.params.item_name
+    let index_name = this.params.index_name
+
     let scope = {}
     let item = node._new_items[index]
-    scope[this.item_name] = item
-    scope[this.index_name] = index
+    scope[item_name] = item
+    scope[index_name] = index
     child.scope = scope
 
     return child
